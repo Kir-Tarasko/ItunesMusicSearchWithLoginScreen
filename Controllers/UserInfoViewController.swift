ad2762e2c6e32becdf892b6c760609046629973d
Кирилл Тараско <tarasko.kirill@icloud.com>
2022-04-16 17:44:46 +0200

    Added AlbumsViewController

--- Controllers/UserInfoViewController.swift
+++ Controllers/UserInfoViewController.swift
@@ -7,8 +7,6 @@
 
 import UIKit
 
-
 class UserInfoViewController: UIViewController {
-    
     
 }
---
+++ Extensions/UIKit/UIViewController + Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewController + Extension.swift
+//  Itunes music search with login screen
+//
+//  Created by Кирилл Тараско on 16.04.2022.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func createCustomButton(selector: Selector) -> UIBarButtonItem {
+        
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "person.fill"), for: .normal)
+        button.tintColor = .black
+        button.addTarget(self, action: selector, for: .touchUpInside)
+        
+        let menuBarItem = UIBarButtonItem(customView: button)
+        return menuBarItem
+    }
+}
