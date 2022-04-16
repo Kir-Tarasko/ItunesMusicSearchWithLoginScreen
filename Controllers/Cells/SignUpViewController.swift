36ca17abf751bf71fb81c6b8253feb73054b13a5
Кирилл Тараско <tarasko.kirill@icloud.com>
2022-04-16 15:55:27 +0200

    Added signUpViewController

--- Controllers/Cells/SignUpViewController.swift
+++ Controllers/Cells/SignUpViewController.swift
@@ -9,4 +9,228 @@ import UIKit
 
 class SingUpViewController: UIViewController {
     
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private let backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let loginLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Registration"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let firstNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "First Name"
+        return textField
+    }()
+    
+    private let firstNameValidLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Required field"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let secondNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Second Name"
+        return textField
+    }()
+    
+    private let secondNameValidLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Required field"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let ageValidLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Required field"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let phoneNumberTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Phone number"
+        textField.keyboardType = .numberPad
+        return textField
+    }()
+    
+    private let phoneNumberValidLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Required field"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Email"
+        return textField
+    }()
+    
+    private let emailValidLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Required field"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "Password"
+        return textField
+    }()
+    
+    private let passwordValidLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Required field"
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .black
+        button.setTitle("Sign Up", for: .normal)
+        button.tintColor = .white
+        button.layer.cornerRadius = 10
+        button.addTarget(SingUpViewController.self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private var elementsStackView = UIStackView()
+    private let datePicker = UIDatePicker()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setConstraints()
+        setupDelegate()
+        setupDatePicker()
+    }
+    
+    private func setupViews() {
+        title = "Sign Up"
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(backgroundView)
+        
+        elementsStackView = UIStackView(arrangedSubviews: [
+            firstNameTextField, firstNameValidLabel, secondNameTextField, secondNameValidLabel, datePicker, ageValidLabel, phoneNumberTextField, phoneNumberValidLabel, emailTextField, emailValidLabel, passwordTextField, passwordValidLabel],
+                                        axis: .vertical,
+                                        spacing: 10,
+                                        distribution: .fillProportionally)
+        backgroundView.addSubview(elementsStackView)
+        backgroundView.addSubview(loginLabel)
+        backgroundView.addSubview(signUpButton)
+    }
+    
+    private func setupDelegate() {
+        firstNameTextField.delegate = self
+        secondNameTextField.delegate = self
+        phoneNumberTextField.delegate = self
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+    }
+    
+    private func setupDatePicker() {
+        datePicker.datePickerMode = .date
+        datePicker.backgroundColor = .white
+        datePicker.layer.borderColor = .init(gray: 0, alpha: 0)
+        datePicker.layer.borderWidth = 1
+        datePicker.clipsToBounds = true
+        datePicker.layer.cornerRadius = 6
+        datePicker.tintColor = .black
+    }
+    
+    @objc private func signUpButtonTapped() {
+        print("SignUPTap")
+    }
+    
+}
+
+extension SingUpViewController: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return false
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        firstNameTextField.resignFirstResponder()
+        secondNameTextField.resignFirstResponder()
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        return true
+    }
+    
+}
+
+extension SingUpViewController {
+    
+     private func setConstraints() {
+        
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+        
+        NSLayoutConstraint.activate([
+            elementsStackView.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor),
+            elementsStackView.centerYAnchor.constraint(equalTo: backgroundView.centerYAnchor),
+            elementsStackView.leadingAnchor.constraint(equalTo: backgroundView.leadingAnchor, constant: 20),
+            elementsStackView.trailingAnchor.constraint(equalTo: backgroundView.trailingAnchor, constant: -20)
+            
+        ])
+        
+        NSLayoutConstraint.activate([
+            backgroundView.centerYAnchor.constraint(equalTo: scrollView.centerYAnchor),
+            backgroundView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            backgroundView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+            backgroundView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+            
+        ])
+        
+        NSLayoutConstraint.activate([
+            loginLabel.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor),
+            loginLabel.bottomAnchor.constraint(equalTo: backgroundView.topAnchor, constant: -30)
+        ])
+        
+        NSLayoutConstraint.activate([
+            signUpButton.centerXAnchor.constraint(equalTo: backgroundView.centerXAnchor),
+            signUpButton.topAnchor.constraint(equalTo: elementsStackView.bottomAnchor, constant: 30),
+            signUpButton.heightAnchor.constraint(equalToConstant: 40),
+            signUpButton.widthAnchor.constraint(equalToConstant: 300)
+        ])
+        
+    }
+    
 }
